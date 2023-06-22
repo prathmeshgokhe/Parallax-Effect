2ceef346bd1bde02b66e6bd6edf40c09ce3bbf72
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-06-22 10:59:24 +0530

    completed

---
+++ src/TextBlock.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./textblock.css"
+
+const TextBlock = () => {
+    return (
+        <>
+            <div id='textblock'>
+                <div id='textblock-container'>
+                    <h1 id='textblock-title'>What is Fire watch</h1>
+                    <p id='textblock-content'>The year is 1989.
+
+                        You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, it's your job to find smoke and keep the wilderness safe.
+
+                        An especially hot, dry summer has everyone on edge. Your supervisor, a woman named Delilah, is available to you at all times over a small, handheld radio—and is your only contact with the world you've left behind.
+
+                        But when something strange draws you out of your lookout tower and into the world below, you'll explore a wild and unknown environment, facing questions and making interpersonal choices that can build or destroy the only meaningful relationship you have.</p>
+                    <div id='textblock-devsense'></div>
+                    <div id='textblock-footer'>Designed by me</div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default TextBlock
